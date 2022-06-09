8c5b49c1ca34420558c14955f200e2d6a4b76cf2
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-09 13:46:01 +0530

    Add files via upload

---
+++ Programs/Programe1/Demo1.java
@@ -0,0 +1,45 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		WAP to accept student name, rollno , 
+//		marks of 5 subject calculate Percentage and display the output
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Name :- ");
+		String name = sc.nextLine();
+		
+		System.out.println("Enter your Roll No. :- ");
+		int rooln = sc.nextInt();
+		
+		System.out.println("Enter your Subject Marks  :- ");
+		
+		System.out.println("English = ");
+		int eng = sc.nextInt();
+		
+		System.out.println("Hindi = ");
+		int hindi = sc.nextInt();
+		
+		System.out.println("Maths = ");
+		int math = sc.nextInt();
+		
+		System.out.println("SST =  ");
+		int sst = sc.nextInt();
+		
+		System.out.println("Science =  ");
+		int science = sc.nextInt();
+		
+		
+		int t = eng+hindi+math+sst+science;
+		System.out.println("Percentage Marks = "+t*100/500+"%");
+		
+		
+		
+	}
+
+}
