ae41f2032eff75ddd842a515b8f97f194caed584
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-10 14:09:14 +0530

    Add files via upload

---
+++ Programs/Write a program to accept a character from user and check it is vowel or not using switch case./hw2.java
@@ -0,0 +1,35 @@
+package Anudip_programe;
+
+import java.util.Scanner;
+
+public class hw2 {
+
+	public static void main(String[] args) {
+		// Write a program to accept a character from user and check it is vowel or not using switch case.
+		
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the Character :-");
+	      String ch = sc.next();
+	      switch(ch) {
+	         case "a" :
+	         System.out.println(ch+" is Vowel");
+	         break;
+	         case "e" :
+	         System.out.println(ch+" is Vowel");
+	         break;
+	         case "i" :
+	         System.out.println(ch+" is Vowel");
+	         break;
+	         case "o" :
+	         System.out.println(ch+" is Vowel");
+	         break;
+	         case "u":
+	        	 System.out.println(ch+" is Vowel");
+	         break;
+	         default :
+	         System.out.println("You Entered Consunent Character");
+	      }
+
+	}
+
+}
