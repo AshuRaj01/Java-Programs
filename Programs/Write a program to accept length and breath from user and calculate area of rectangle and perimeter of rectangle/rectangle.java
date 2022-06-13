6be18aee6ca5f50ed9bb326aea3c6554b172a023
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-13 09:53:03 +0530

    Add files via upload

---
+++ Programs/Write a program to accept length and breath from user and calculate area of rectangle and perimeter of rectangle/rectangle.java
@@ -0,0 +1,25 @@
+package Anudip_programe;
+
+import java.util.Scanner;
+
+public class rectangle {
+
+	public static void main(String[] args) {
+		
+		//Write a program to accept length and breath from user and calculate area of rectangle and perimeter of rectangle
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Value of Length = ");
+		int l = sc.nextInt();
+		
+		System.out.print("Enter the breath = ");
+		int b = sc.nextInt();
+  
+        int area = l*b;
+        System.out.println("The Area of Rectangle = "+area);
+        
+        int perimeter = 2*(l+b);
+        System.out.println("The perimeter of Rectangle = "+perimeter);
+	}
+
+}
