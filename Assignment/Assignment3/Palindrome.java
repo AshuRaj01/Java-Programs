b337062729e7a89909a7dd64a340b7f1ad134ab9
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-16 23:20:41 +0530

    Create Palindrome.java

---
+++ Assignment/Assignment3/Palindrome.java
@@ -0,0 +1,32 @@
+package Assisment;
+
+import java.util.Scanner;
+
+public class a3 {
+
+	public static void main(String[] args) {
+		// WAP to accept a no from user and check it is palindrom or not .
+		palindrome();// using Function.
+	}
+	
+	//Creating Function 
+	 public static void palindrome()
+	 {  
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Enter the No. = ");
+		 int n= sc.nextInt();
+		  int r,sum=0,temp;    
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println(sum+" "+" is Palindrome number ");    
+		  else    
+		   System.out.println("Not palindrome");    
+		}  
+}
