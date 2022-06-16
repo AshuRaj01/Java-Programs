04637cbb5953e6fcfbc1388a961c215604484ef1
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-16 23:37:10 +0530

    Update and rename abc to p1.java

---
+++ Programs/Patterns/P1/p1.java
@@ -0,0 +1,22 @@
+package loops;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+// ****
+// ****
+// ****
+// ****
+
+    //rows
+		for(int i = 1;i<=4;i++) {
+      
+      //column
+			for(int j = 1;j<=4;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
