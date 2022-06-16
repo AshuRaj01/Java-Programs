8d830ce2b9d838ef1cadad3b143285c03f3f111a
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-16 23:46:36 +0530

    Update and rename Programs/Patterns/abc to Programs/Patterns/P6/p6.java

---
+++ Programs/Patterns/P6/p6.java
@@ -0,0 +1,28 @@
+package loops;
+
+public class p6{
+
+	public static void main(String[] args){
+//    *
+//   ***
+//  *****
+// *******
+//*********
+		int row =5;
+		//for row
+		for(int i = 1;i<=5;i++) {
+			// for space
+			for(int k = 5;k>i;k--) {
+				System.out.print(" ");
+			}
+			// for columns
+			for(int j = 1;j<=i*2-1;j++) {// i*2-1 for odd numbers
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+			
+	}
+	
+}
+
