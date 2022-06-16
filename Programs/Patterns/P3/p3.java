a589a5ccc3edce21a93c4801c52d9a8ea12aca79
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-16 23:41:36 +0530

    Update and rename abc to p3.java

---
+++ Programs/Patterns/P3/p3.java
@@ -0,0 +1,31 @@
+package loops;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//        1
+//        10
+//        101
+//        1010
+//        10101
+    
+    //rows
+		for(int i = 1;i<=5;i++) {
+      
+      //columns
+			for(int j = 1;j<=i;j++) {
+        
+        //for 1 or 0
+				if(j%2==0) {
+					System.out.print("0");
+				}else {
+					System.out.print("1");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
+
