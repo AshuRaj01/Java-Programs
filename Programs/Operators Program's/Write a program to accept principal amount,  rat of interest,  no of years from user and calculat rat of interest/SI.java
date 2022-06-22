3d41c5a8fe37fbe6d3dce744e9e9727f52765de8
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-06-22 08:04:00 +0530

    Create SI.java

---
+++ Programs/Operators Program's/Write a program to accept principal amount,  rat of interest,  no of years from user and calculat rat of interest/SI.java
@@ -0,0 +1,24 @@
+package Anudip_programe;
+
+import java.util.Scanner;
+
+public class SI {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Write a program to accept principal amount,  rat of interest,  no of years from user and calculat rat of interest
+		System.out.print("Enter the Pricipal Amount = ");
+		float p = sc.nextFloat();
+		
+		System.out.print("Enter the Time = ");
+		float t = sc.nextFloat();
+		
+		System.out.print("Enter the Rate of Interest = ");
+		float r = sc.nextFloat();
+  
+        /* Calculate simple interest */
+        float SI = (p * t * r) / 100;
+        System.out.println("Simple interest = " + SI);
+	}
+
+}
