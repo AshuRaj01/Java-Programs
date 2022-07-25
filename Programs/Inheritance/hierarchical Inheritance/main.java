e385c882719add657cde46db68c3a786f864ed6c
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-07-25 10:31:03 +0530

    Update and rename abc to main.java

---
+++ Programs/Inheritance/hierarchical Inheritance/main.java
@@ -0,0 +1,23 @@
+package hierarchical_Inheritance;
+
+import java.util.Scanner;
+
+public class main {
+
+     	static int n;
+		
+		public static int accept()
+		{
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter the Digit : ");
+			n = sc.nextInt();
+			System.out.println("Digit : "+n);
+			return n;
+		}
+		
+	}
+	
+	
+
+
