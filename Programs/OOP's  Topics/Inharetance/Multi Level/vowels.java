4635d61a4b6a1ae3f698b9df5dc431a1e3bd89d8
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-07-29 11:06:31 +0530

    Create vowels.java

---
+++ Programs/OOP's  Topics/Inharetance/Multi Level/vowels.java
@@ -0,0 +1,34 @@
+
+// 3rd Step
+
+package multi_level_inheritance;
+
+public class vewols extends grt_num
+{
+
+	public static void vowel()
+	{
+		System.out.print("Enter the Character = ");
+		 alph = sc.next().charAt(0);
+		
+		switch(alph) {
+		case 'a' :
+	         System.out.println("Vowel");
+	         break;
+	         case 'e' :
+	         System.out.println(" Vowel");
+	         break;
+	         case 'i' :
+	         System.out.println(" Vowel");
+	         break;
+	         case 'o' :
+	         System.out.println(" Vowel");
+	         break;
+	         case 'u':
+	        	 System.out.println(" Vowel");
+	         break;
+	         default :
+	         System.out.println("You Entered Consunent Character");
+		}
+	}
+}
