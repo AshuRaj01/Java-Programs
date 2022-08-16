bb3ee13ac91867be1741ee6a88240bb796cd1e81
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2022-08-16 11:59:15 +0530

    Create Q2.java

---
+++ Assignment/Thu(11-08-22)/Question_2/Q2.java
@@ -0,0 +1,7 @@
+/*Q2 Write a program to do the following process 
+-Number of Duplicate Words in String
+-Number of Words in Given String
+-Occurrences of Each Character in String
+-Remove Duplicate Words from String 
+-Count of Vowels*/
+
